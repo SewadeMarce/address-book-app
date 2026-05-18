b0eca4d265a520ed24a3107e407a848c09b2dc13
Marce <marcekekey@gmail.com>
2026-05-18 23:39:02 +0100

    ok route

--- app/routes.ts
+++ app/routes.ts
@@ -5,12 +5,5 @@ export default [
     layout("auth/lajout.tsx", [
         route("/auth", "auth/page.tsx")
     ]),
-  
-        layout("carnet-adresses/layout.tsx", [
-            index("carnet-adresses/page.tsx"),
-            route("new", "carnet-adresses/new/page.tsx"),
-            route(":id", "carnet-adresses/:id/page.tsx"),
-            route(":id/edit", "carnet-adresses/:id/edit/page.tsx"),
-        ]),
 
 ] satisfies RouteConfig;
